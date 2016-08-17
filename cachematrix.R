88536c0798ac9a900687fc13068f482aa5ae79d9
Simonlo1 <lawyer70130@gmail.com>
2016-08-17 17:20:10 -0500

    Assignment 2
    This is the completed R file of coursera R Programming course assignment
    2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve are used together to optimize the calculation
+## of inverted matrix by caching previously calculated result.
+##
+## makeCacheMatrix create a special matrix object (list):
+##      sm <- makeCacheMatrix(x) : Return a special matrix object initialized with x
+##      sm$set(x) : Set storedd matrix to x and clear cached inverted result
+##      sm$get() : Return the original stored matrix object
+##      sm$setinverse(ix) : Store a calculated inverted matrix. Used by cacheSolve
+##      sm$getinverse() : return the stored inverted matrix. Used by cacheSolve
+##
+## cacheSolve return the inverted matrix by calculating it or, if
+##    it as already been calculated, using previously calculated result:
+##      cacheSolve(sm) : Return the inverted matrix of sm
+## 
+## Usage :
+## 1. Initialise special matrix object by using sm <- makeCacheMatrix(m) where
+##    m is a square invertible matrix.
+##
+## 2. cacheSolve(sm) will return the inverted matrix by calculating it or, if
+##    it as already neen calculated, the previously calculated result.
 
-## Write a short comment describing this function
+## makeCacheMatrix return a special matrix object (list) to store the matrix
+## and its cached inverse is it as already been calculated.
 
 makeCacheMatrix<-function(x=matrix()){
   inv<-NULL
@@ -15,8 +34,8 @@ makeCacheMatrix<-function(x=matrix()){
   list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-## Write a short comment describing this function
-
+## cacheSolve take a special matrix object created by makeCacheMatrix and
+## calculate or return the previsously calculated matrix inverse.
 
 cacheSolve<-function(x,...){
   inv<-x$getinverse()
